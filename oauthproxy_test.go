ad1571b926575932a528801810b7ce191ec3dfd5
einfachchr <einfachchr@gmail.com>
2018-11-09 09:41:19 +0100

    fixed imports

--- oauthproxy_test.go
+++ oauthproxy_test.go
@@ -16,7 +16,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/bitly/oauth2_proxy/providers"
+	"github.com/einfachchr/oauth2_proxy/providers"
 	"github.com/mbland/hmacauth"
 	"github.com/stretchr/testify/assert"
 )
